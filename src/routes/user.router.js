00b5355526feabebce745d96202157f2aa96c215
Johann Rodriguez Abreu <johann2464@gmail.com>
2023-08-14 23:21:57 -0400

    Login , Verificacion

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,15 +1,19 @@
-const { getAll, create, remove, update } = require('../controllers/user.controller');
+const { getAll, create, remove, update, login } = require('../controllers/user.controller');
 const express = require('express');
+const verifyJWT = require('../utils/verifyJWT');
 
 const routerUser = express.Router();
 
 routerUser.route('/')
-  .get(getAll)
+  .get(verifyJWT, getAll)
   .post(create);
 
+routerUser.route('/login')
+  .post(login);
+
+
 routerUser.route('/:id')
-  .get()
-  .delete(remove)
-  .put(update);
+  .delete(verifyJWT, remove)
+  .put(verifyJWT, update);
 
 module.exports = routerUser;
